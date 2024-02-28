f347a43c4684302df17f1da86d064ad5fabeda31
normalnymarek <jarolewski9@gmail.com>
2024-02-28 13:16:53 +0100

    Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -23,3 +23,27 @@ plt.figure(figsize=(30,20))
 sns.set(font_scale=1.3)
 sns.heatmap(triangle,annot=True,fmt=".1%",cmap="RdYlGn",vmin=-0.4,vmax=0.15,center=0)
 plt.tick_params(axis="y",labelright=True)
+
+
+#for 100 PLN (or other for fixet FX):
+
+ticker = ["MSFT"]
+df = yf.download(ticker,start="2000-01-01",end="2023-12-31")
+df_closed = df.loc[:,"Close"].to_frame()
+roczny = df_closed.resample(rule="A", kind = "period").last()
+lata = year.index.size
+roczny["zwrot"] = np.log(roczny.Close/roczny.Close.shift(periods=1))
+roczny.dropna(inplace=True)
+windows = [year for year in range(years,0,-1)]
+
+for rok in windows:
+    roczny[f"{rok}Y"] = np.exp(rok*roczny.zwrot.rolling(rok).mean()) * 100
+
+traingle = roczny.drop(columns=["Close","zwrot"])
+plt.figure(figsize=(30,20))
+sns.set(font_scale=1.3)
+sns.heatmap(traingle, annot=True, fmt=".0f", cmap="RdYlGn",vmin=70,vmax=200, center=100)
+plt.tick_params(axis="y",labelright=True)
+
+
+
